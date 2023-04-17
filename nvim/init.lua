6d633fb0e1a712239788eefa5270a1da12eddef8
kuromika <kuromika.dev@gmail.com>
2023-04-17 12:48:08 -0500

    Convert nvim to submodule again

--- nvim/init.lua
+++
@@ -1,9 +0,0 @@
-vim.g.mapleader = ","
-require('base')
-require('plugins')
-require('highlights')
-require('maps')
-
-vim.opt.background = 'dark'
-vim.opt.clipboard:append {'unnamedplus'}
-vim.opt.title = false;
